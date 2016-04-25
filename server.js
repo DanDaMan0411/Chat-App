72b82b99a3d5114075a05cf59c715515e21e95d5
DanDaMan0411 <dan.kirichok@gmail.com>
2016-04-25 00:52:00 -0400

    added openshift variables

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ io.on('connection', function(socket){
 	})
 })
 
-http.listen(4000, function(){
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+http.listen(port, ipaddress, function(){
 	console.log('listening on *:4000')
 })
